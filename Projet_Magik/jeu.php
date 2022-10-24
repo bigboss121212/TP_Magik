b0c5a48809623646c0aac82376fa4ea6a313e66a
bigboss121212 <caron.alex18@hotmail.fr>
2022-10-24 17:53:12 -0400

    infoJoueur

--- Projet_Magik/jeu.php
+++ Projet_Magik/jeu.php
@@ -8,7 +8,13 @@
 
 	$pageTitre = "Jeu";
 	require_once("partial/header.php");
+
+
 ?>
+		<div id="jeuAdversaire"></div>
+
+		<div id="background_3"></div>
+	
 
 		<div class="grid-container">
 		<div class="grid-item">1</div>
@@ -18,10 +24,23 @@
 		<div class="grid-item">5</div>
 		<div class="grid-item">6</div>
 		<div class="grid-item">7</div>
-		<div class="grid-item">8</div>
-		<div class="grid-item">9</div>
 		</div>
 
+		<div id="classH"></div>
+
+		<div id="infoPerso">
+			<div id="nbCarte"></div>
+			<div id="temps"></div>
+			<div id="vie"></div>
+			<div id="magie"></div>
+		</div>
+
+		<div id="jeuPersonnel"></div>
+
+
+		<div id="abandonne">
+			<div id="boutonA"></div>	
+		</div>
 
 	</body>
 </html>
--- Projet_Magik/js/jeu.js
+++ Projet_Magik/js/jeu.js
@@ -7,16 +7,41 @@ const state = () => {
     .then(response => response.json())
     .then(data => {
         
+
         console.log(data); // contient les cartes/état du jeu.
 
+        gameUpdate(data);
+        
         setTimeout(state, 1000); // Attendre 1 seconde avant de relancer l’appel
     })
 
 }
 
 window.addEventListener("load", () => {
+
 setTimeout(state, 1000); // Appel initial (attendre 1 seconde)
+
 });
+
+const gameUpdate = data => {
+
+    console.log(data.hp);
+
+    let classH = document.getElementById("classH");
+    let vie = document.getElementById("vie");
+    let temps = document.getElementById("temps");
+    let magie = document.getElementById("magie");
+    let nbCarte = document.getElementById("nbCarte");
+
+    classH.innerHTML = data.heroClass;
+    vie.innerHTML = "vie: " + data.hp;
+    temps.innerHTML = "temps: " + data.remainingTurnTime;
+    magie.innerHTML = "mp: " + data.mp;
+    nbCarte.innerHTML = "carte: " + data.remainingCardsCount;
+
+}
+
+
 
 
 
