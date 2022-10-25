a8f03225237483cedd6dea6506de7bee51f6970d
bigboss121212 <caron.alex18@hotmail.fr>
2022-10-25 07:38:16 -0400

    affichageCarte

--- Projet_Magik/jeu.php
+++ Projet_Magik/jeu.php
@@ -15,15 +15,64 @@
 
 		<div id="background_3"></div>
 	
-
-		<div class="grid-container">
-		<div class="grid-item">1</div>
-		<div class="grid-item">2</div>
-		<div class="grid-item">3</div>
-		<div class="grid-item">4</div>
-		<div class="grid-item">5</div>
-		<div class="grid-item">6</div>
-		<div class="grid-item">7</div>
+		<!-- retravailler les carte -->
+		<div id="grid-container">
+			<div class="grid-item">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+			<div class="grid-item">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+			<div class="grid-item">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+			<div class="grid-item">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+			<div class="grid-item">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+			<div class="grid-item">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+			<div class="grid-item">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
 		</div>
 
 		<div id="classH"></div>
--- Projet_Magik/js/jeu.js
+++ Projet_Magik/js/jeu.js
@@ -25,19 +25,51 @@ setTimeout(state, 1000); // Appel initial (attendre 1 seconde)
 
 const gameUpdate = data => {
 
-    console.log(data.hp);
+    if(data.hand != null){
+        let classH = document.getElementById("classH");
+        let vie = document.getElementById("vie");
+        let temps = document.getElementById("temps");
+        let magie = document.getElementById("magie");
+        let nbCarte = document.getElementById("nbCarte");
 
-    let classH = document.getElementById("classH");
-    let vie = document.getElementById("vie");
-    let temps = document.getElementById("temps");
-    let magie = document.getElementById("magie");
-    let nbCarte = document.getElementById("nbCarte");
+        classH.innerHTML = data.heroClass;
+        vie.innerHTML = "vie: " + data.hp;
+        temps.innerHTML = "temps: " + data.remainingTurnTime;
+        magie.innerHTML = "mp: " + data.mp;
+        nbCarte.innerHTML = "carte: " + data.remainingCardsCount;
 
-    classH.innerHTML = data.heroClass;
-    vie.innerHTML = "vie: " + data.hp;
-    temps.innerHTML = "temps: " + data.remainingTurnTime;
-    magie.innerHTML = "mp: " + data.mp;
-    nbCarte.innerHTML = "carte: " + data.remainingCardsCount;
+        console.log(data.hand);
+        i = 0;
+        data.hand.forEach(element => {
+            
+            
+            let main = document.getElementById("grid-container");
+            let carte = document.getElementById("grid-container").childNodes[i+1];
+
+            // let div = document.createElement('div');
+            // div.className = "grid-item";
+
+            // main.prepend(div);
+            // console.log(document.getElementById("grid-container").childNodes[i]);
+            // console.log( element.atk);
+            carte.childNodes[1].textContent = "atk: " + element.atk;
+            carte.childNodes[3].textContent = "baseHP: " + element.baseHP;
+            carte.childNodes[5].textContent = "cost: " + element.cost;
+            carte.childNodes[7].textContent = "dedicated: " + element.dedicated;
+            carte.childNodes[9].textContent = "hp: " + element.hp;
+            carte.childNodes[11].textContent = "id: " + element.id;
+            // carte.textContent = element.atk;
+            // carte.textContent = element.baseHP;
+            // carte.innerHTML = element.cost;
+            // carte.innerHTML = element.dedicated;
+            // carte.innerHTML = element.hp;
+            // carte.innerHTML = element.id;
+            
+            i = i + 2;
+
+        });
+    }
+
 
 }
 
